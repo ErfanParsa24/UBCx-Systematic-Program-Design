9a4d2f38f5587f0458673488e460ff4964c6c184
ErfanParsa24 <nikanerfan90@gmail.com>
2024-06-04 23:48:39 +0430

    first mudule completed

---
+++ 1a-beginning-student-language/3-Strings and Images/string in contex.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |string in contex|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+"apple"
+(string-append "Ada"  " " "Lovelace")
+(string-length "apple")
+(substring "caribou" 2 4)
+(substring "012345678" 2 4)
